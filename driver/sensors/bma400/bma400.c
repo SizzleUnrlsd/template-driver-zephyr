89a67eba1b3a9b0f17ab9dc1c08379243069a61f
SizzleUnrlsd <hugo.payet@epitech.eu>
2024-10-04 10:57:11 +0400

    Feat: adding driver config

---
+++ driver/sensors/bma400/bma400.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 Arribada Initiative CIC
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "bma400.h"
+
+int main(void)
+{
+    return 0;
+}
---
+++ driver/sensors/bma400/bma400.h
@@ -0,0 +1,5 @@
+/*
+ * Copyright (c) 2023 Arribada Initiative CIC
+ *
+ * SPDX-License-Identifier: MIT
+ */
