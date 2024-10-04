02971aef2c223efd094b079fb1d3a7d660a29f47
SizzleUnrlsd <hugo.payet@epitech.eu>
2024-10-04 11:00:23 +0400

    Feat: adding example sample

---
+++ examples/simple/src/main.c
@@ -0,0 +1,11 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/shell/shell.h>
+
+int main(void)
+{
+    return 0;
+}
